3845956d8a39250a4a03cec9698dff97bf1545f2
manganarofabio <manganarofabio@gmail.com>
2020-11-29 16:44:13 +0100

    Update functionalities

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Calculator
     {
 
         double lastNumber, result;
+        SelectedOperator selectedOperator;
 
         public MainWindow()
         {
@@ -37,15 +38,48 @@ namespace Calculator
 
         private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber)) {
+
+                switch (selectedOperator) {
+
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        result = SimpleMath.Sub(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                   default:
+                        break;
+
+                }
+
+                resultLabel.Content = result.ToString();
+            
+            }
+
+
+
         }
 
         private void PercentageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber)) {
 
-                lastNumber /= 100;
-                resultLabel.Content = lastNumber.ToString();
+            double tmpNumber;
+
+            if (double.TryParse(resultLabel.Content.ToString(), out tmpNumber)) {
+
+                tmpNumber /= 100;
+                if (lastNumber != 0)
+                    tmpNumber *= lastNumber;
+                resultLabel.Content = tmpNumber.ToString();
             }
         }
 
@@ -65,21 +99,38 @@ namespace Calculator
         private void AcButton_Click(object sender, RoutedEventArgs e)
         {
             resultLabel.Content = "0";
+            result = 0;
+            lastNumber = 0;
         }
 
         private void OperationButton_Click(object sender, RoutedEventArgs e)
         {
 
-            if(sender == divideButton)
+            
+            
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
             {
-                if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
-                {
-                    resultLabel.Content = "0";
-                }
-
+                resultLabel.Content = "0";
             }
+            
+
+            if (sender == multiplyButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divideButton)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == addButton)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == subtractButton)
+                selectedOperator = SelectedOperator.Substraction;
 
 
+
+        }
+
+        private void dotButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!resultLabel.Content.ToString().Contains("."))
+                resultLabel.Content = $"{resultLabel.Content}.";
         }
 
         private void NumberButton_Click(object sender, RoutedEventArgs e)
@@ -95,4 +146,44 @@ namespace Calculator
 
         }
     }
+
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double op1, double op2)
+        {
+            return op1 + op2;
+        }
+
+        public static double Sub(double op1, double op2)
+        {
+            return op1 - op2;
+        }
+
+        public static double Multiply(double op1, double op2)
+        {
+            return op1 * op2;
+        }
+
+        public static double Divide(double op1, double op2)
+        {
+            if (op2 != 0)
+                return op1 / op2;
+            else
+            {
+                MessageBox.Show("Divisiono by 0 is not allowed", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0; 
+            }
+        }
+    }
+
+
 }
